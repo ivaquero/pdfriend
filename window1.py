4f2202e2d59c9bd7074cceccb3337c8592a7ba83
ivaquero <msaintms@outlook.com>
2022-08-26 19:38:33 +0800

    feat: 💄 add ui

---
+++ window1.py
@@ -0,0 +1,24 @@
+﻿import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(200, 200, 700, 400)
+        self.setWindowTitle('PyQt6')
+        # self.setFixedHeight(400)
+        # self.setFixedWidth(700)
+
+        self.setStyleSheet('background-color: white')
+        # self.setWindowOpacity(0.8)
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+
+sys.exit(app.exec())
---
+++ window2.py
@@ -0,0 +1,12 @@
+﻿import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.statusBar().showMessage("Welcome")
+window.menuBar().addMenu("File")
+
+window.show()
+
+sys.exit(app.exec())
